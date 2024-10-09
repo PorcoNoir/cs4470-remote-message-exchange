3c752687d529e564ed26128a2c5cea91f34c729d
Yvette Sevillano <ysevill94@gmail.com>
2024-10-08 21:11:20 -0700

    Added base functionality for switching between multiple threads, still working on integrating the socket_implementation functions into file.

--- multithreaded_sockets.py
+++ multithreaded_sockets.py
@@ -1,5 +1,7 @@
+import socket
 import threading
 import queue
+
 
 class WorkerThread(threading.Thread):
     """ Worker thread is a wrapper for Threads and handles the 
@@ -9,10 +11,36 @@ class WorkerThread(threading.Thread):
     """
     def __init__(self, port='4545', ip='0.0.0.0'):
         super().__init__()
-        self.event_queue = queue.Queue() 
+        self.command_queue = queue.Queue()
         self.running = True
-        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.start()  
+        self.event_hander = threading.Event()
+        self.connection_list = {}
+        
+        #TODO: might be nice to replace this with our own TcpSocket wrapper that way we can add custom methods/properties
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        
+        # ready to start thread
+        print("Starting worker thread and going back to cli")
+        self.start()
+        self.event_hander.set()  
+
+    def set_event_handle():
+        if self.event_handler.is_set():
+            pass
+        else:
+            self.event_handler.set()
+        
+    # TODO: still have to see how to implement this.
+    # def from_cli(self, event):
+    #     """ function receives event from the cli, will execute the command 
+    #     then gives back control to cli to continue to wait for user input
+    #     """
+    def list_connections(self):
+        print("TBD: TEMPLATE FOR CONNECTIONS LIST")
+        return        
+
+    def _add_connection(self, dest_address, port):
+        connect(dest_address, port)  # from socket_implementation code
 
     def add_connection(self, sockfd):
         # handles adding socket fd to a new thread
@@ -22,15 +50,44 @@ class WorkerThread(threading.Thread):
     def run(self):
         while self.running:
             try:
-                event = self.event_queue.get_nowait()  # Wait for an event with a timeout
-                self._event_message_rx(event)
+                event = self.command_queue.get(block=False, timeout=1)  # Wait for an event with a timeout
+                self._process_event(event)
             except queue.Empty:
-                self.event_queue.clear()
+                # self.command_queue.clear()
                 pass  # No event received within the timeout
 
     def send_message(self, message):
-        self.event_queue.put(message)
+        self.command_queue.put(message)
 
+    def _process_event(self, event):
+        # Process the event here
+        if ("myip" in event):
+            print("myip()")
+        elif ("myport" in event):
+            print("myport()")
+        elif ("connect" in event):
+            # event[0] should be "connect"
+            try:
+                destination_address = event[1]
+                port = event[2]
+                self._add_connection(destination_address, port)
+            except:
+                print("connect()")
+        elif ("list" in event):
+            print("list()")
+            for val in self.connection_list:
+                print(val)
+        elif ("terminate" in event):
+            print("terminate()")
+        elif ("send" in event):
+            print("Message received:", event)
+        elif ("exit" in event):
+            print("Closing all socket connections")
+            self.stop()
+        else:
+            print("Not a supported command, please type help")
+
+## TCP Socket methods 
     def _event_message_rx(self, event):
         # Process the event here
         print("Message received:", event)
@@ -46,6 +103,10 @@ class WorkerThread(threading.Thread):
     def _event_socket_close(self, event, sockfd):
         # Process the event here
         print("Closing connection:", event)
+        
+    def _terminate_connection(self, connection_id):
+        # function to terminate connection_id socket
+        return 0
 
     def stop(self):
         # should send close request to all connections that are open
@@ -56,10 +117,14 @@ if __name__ == "__main__":
     # start thread manager
     socket_manager = WorkerThread()
 
-    socket_manager.send_message("Event 1")
-    socket_manager.send_message("Event 2")
+    socket_manager.send_message([0, "Event 1"])
+    socket_manager.send_message("myip")
+    socket_manager.send_message("myport")
+    socket_manager.send_message("terminate")
+    socket_manager.send_message("connect")
+    socket_manager.send_message("list")
+    socket_manager.send_message("exit")
     # Simulate sending events
 
     # Wait for worker to finish processing events
     socket_manager.join()
-    socket_manager.stop()
